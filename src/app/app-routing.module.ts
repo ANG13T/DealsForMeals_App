b3a2d5c92740f68fae2a94dc222a7ca0ec1e3f74
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-10 03:01:52 +0900

    feat: reconfig comp. and rerouted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePage } from './home/home.page';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    component: HomePage
   },
 ];
 
