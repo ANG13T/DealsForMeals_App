a9b8df09483cb848419d895297864004e60790e3
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-10 03:35:20 +0900

    feat: init new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { HomePage } from './home/home.page';
+import { HomePage } from './components/home/home.page';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent
+  },
+  {
+    path: 'login',
+    component: SignInComponent
   },
 ];
 
