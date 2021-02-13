fa3f0271baf71538b7921c4a09974b68cb78326b
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-13 09:55:01 +0900

    feat: added some func to edit profile

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -10,6 +10,7 @@ import { UserService } from 'src/app/shared/services/user.service';
 })
 export class EditProfileComponent implements OnInit {
   userProfile: User = {name: "", email: "", accountType: "foodbank", photoURL: "", uid: "", location: ""};
+  initUserProfile: User = {name: "", email: "", accountType: "foodbank", photoURL: "", uid: "", location: ""};
   errors = {password: "", name: "", email: ""};
 
   constructor(private router: Router, private userService: UserService) {
@@ -20,8 +21,20 @@ export class EditProfileComponent implements OnInit {
         this.userProfile.email = userProfile.email;
         this.userProfile.photoURL = userProfile.photoURL;
         this.userProfile.accountType = userProfile.accountType;
+
+        //init user profile
+        this.initUserProfile.name = userProfile.name;
+        this.initUserProfile.email = userProfile.email;
+        this.initUserProfile.photoURL = userProfile.photoURL;
+        this.initUserProfile.accountType = userProfile.accountType;
       }
     })
+  }
+
+  editProfile(){
+    if(this.validateForm()){
+      alert("Completed");
+    }
   }
 
   ngOnInit() {}
@@ -29,5 +42,36 @@ export class EditProfileComponent implements OnInit {
   navigate(route: string){
     this.router.navigate([`/${route}`]);
   }
+
+  fieldsDifferent(){
+    return JSON.stringify(this.initUserProfile) === JSON.stringify(this.userProfile) ;
+  }
+
+  validateForm(): boolean{
+    let result = true;
+    if(this.userProfile.name.length > 30){
+      this.errors.name = "Name must be shorter than 30 letters";
+      result = false;
+    }
+
+    if(this.userProfile.name.length < 5){
+      this.errors.name = "Name must be longer than 5 letters";
+      result = false;
+    }
+
+    if(!this.validateEmail(this.userProfile.email)){
+      this.errors.email = "Invalid Email";
+      result = false;
+    }
+
+    return result;
+  }
+
+  validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+
 
 }
