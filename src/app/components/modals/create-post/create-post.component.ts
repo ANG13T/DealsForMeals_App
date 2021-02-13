fb46974b108143e2929de96204277201886813de
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-13 22:33:01 +0900

    feat: created all elements for create post

--- src/app/components/modals/create-post/create-post.component.ts
+++ src/app/components/modals/create-post/create-post.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+import { Post } from 'src/app/shared/models/post.model';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-create-post',
@@ -9,9 +11,20 @@ import { ModalController } from '@ionic/angular';
 })
 export class CreatePostComponent implements OnInit {
 
-  constructor(private modalController: ModalController, private router: Router) { }
+  post: Post = {title: "", description: "", userProfile: null, images: [], amount: 0};
 
-  ngOnInit() {}
+  constructor(private modalController: ModalController, private router: Router, private userService: UserService) { 
+    this.userService.user$.subscribe((userProfile) => {
+      if(userProfile){
+        this.post.userProfile = userProfile;
+        this.post.images = ["https://upload.wikimedia.org/wikipedia/commons/2/22/Pacific_Walrus_-_Bull_%288247646168%29.jpg"]
+      }
+    })
+  }
+
+  ngOnInit() {
+    
+  }
 
   dismissModal(){
     this.modalController.dismiss({
