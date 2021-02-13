b08ca1677e637f72eb37f5ad4c672f9f8567c088
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-14 01:21:36 +0900

    feat: get post data

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -4,6 +4,8 @@ import { UserService } from 'src/app/shared/services/user.service';
 import { MenuController, ModalController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { CreatePostComponent } from '../modals/create-post/create-post.component';
+import { Post } from 'src/app/shared/models/post.model';
+import { PostService } from 'src/app/shared/services/post.service';
 
 @Component({
   selector: 'app-profile',
@@ -14,9 +16,10 @@ export class ProfileComponent implements OnInit {
 
   user: User;
   loading: boolean = true;
+  posts: Post[] = [];
   isFoodbank: boolean = false;
 
-  constructor(private userService: UserService, private router: Router, public modalController: ModalController) { }
+  constructor(private userService: UserService, private router: Router, public modalController: ModalController, private postService: PostService) { }
 
   ngOnInit() {
     this.loading = true;
@@ -25,8 +28,16 @@ export class ProfileComponent implements OnInit {
       this.user = userProfile;
       if(this.user.accountType == "foodbank"){
         this.isFoodbank = true;
+        await this.postService.getPosts(this.user.uid).then((data) => {
+          console.log("got the posts", data)
+          this.posts = data;
+        })
       }else{
         this.isFoodbank = false;
+        await this.postService.getPosts(this.user.uid).then((data) => {
+          console.log("got the posts", data)
+          this.posts = data;
+        })
       }
       this.loading = false;
     });
--- src/app/shared/services/post.service.ts
+++ src/app/shared/services/post.service.ts
@@ -19,4 +19,17 @@ export class PostService {
       return err;
     })
   }
+
+  async getPosts(uid: string): Promise<any>{
+    let posts: Post[] = [];
+    let promise = this.afs.firestore.collection("posts").where('userProfile.uid', '==', uid).get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        let docData = doc.data();
+        let newPost: Post = {userProfile: docData.userProfile, title: docData.title, description: docData.description, images: docData.images, amount: docData.amount};
+        posts.push(newPost);
+      })
+      return posts;
+    });
+    return promise;
+  }
 }
