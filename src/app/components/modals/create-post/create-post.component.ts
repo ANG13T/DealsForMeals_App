78ad717920a0edc7eeda2d470db37a4b6c9b2187
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-14 00:23:46 +0900

    feat: added detail to create post

--- src/app/components/modals/create-post/create-post.component.ts
+++ src/app/components/modals/create-post/create-post.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage, AngularFireUploadTask } from '@angular/fire/storage';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
+import { finalize } from 'rxjs/operators';
 import { Post } from 'src/app/shared/models/post.model';
 import { PostService } from 'src/app/shared/services/post.service';
 import { UserService } from 'src/app/shared/services/user.service';
@@ -15,13 +17,13 @@ export class CreatePostComponent implements OnInit {
   post: Post = {title: "", description: "", userProfile: null, images: [], amount: 1};
   errors = {title: "", description: "", amount: ""};
   loading: boolean = false;
+  imageLoading: boolean = false;
   complete: boolean = false;
 
-  constructor(private modalController: ModalController, private router: Router, private userService: UserService, private postService: PostService) { 
+  constructor(private modalController: ModalController, private router: Router, private userService: UserService, private postService: PostService, private storage: AngularFireStorage) { 
     this.userService.user$.subscribe((userProfile) => {
       if(userProfile){
         this.post.userProfile = userProfile;
-        this.post.images = ["https://upload.wikimedia.org/wikipedia/commons/2/22/Pacific_Walrus_-_Bull_%288247646168%29.jpg"]
       }
     })
   }
@@ -34,6 +36,42 @@ export class CreatePostComponent implements OnInit {
     this.modalController.dismiss({
       'dismissed': true
     });
+  }
+
+  goToProfile(){
+    //send eveent back with new post data
+    this.dismissModal();
+  }
+
+  onDrop(files: FileList){
+    let profileImage = files[0];
+    this.uploadTask(profileImage);
+  }
+
+  uploadTask(file: File){
+    let task: AngularFireUploadTask;
+    let downloadURL;
+    this.imageLoading = true;
+
+     // The storage path
+    const path = `files/${Date.now()}_${file.name}`;
+     // Reference to storage bucket
+    const ref = this.storage.ref(path);
+    console.log(ref);
+
+     // The main task
+    task = this.storage.upload(path, file);
+
+
+    task.snapshotChanges().pipe(
+       // The file's download URL
+       finalize(async () =>  {
+         downloadURL = await ref.getDownloadURL().toPromise();
+         this.post.images.push(downloadURL);
+         this.imageLoading = false;
+       })
+     ).subscribe();
+
   }
 
   navigate(route: string){
@@ -61,6 +99,11 @@ export class CreatePostComponent implements OnInit {
       return false;
     }
 
+    if(this.imageLoading){
+      alert("Image Uploading...");
+      return false;
+    }
+
     if(this.post.description.length < 10){
       this.errors.description = "Title must be longer than 10 letters";
       return false;
@@ -78,9 +121,6 @@ export class CreatePostComponent implements OnInit {
       await this.postService.createPost(this.post).then((result) => {
         this.loading = false;
         this.complete = true;
-        if(result == "success"){
-          this.dismissModal();
-        }
       })  
     }
   }
