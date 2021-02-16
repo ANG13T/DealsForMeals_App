aacaabb5a7be483e0c217184e9ae6147e47fceaf
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-16 23:53:19 +0900

    feat: added elements to locations

--- src/app/components/locations/locations.component.ts
+++ src/app/components/locations/locations.component.ts
@@ -1,14 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  Marker,
+  GoogleMapsAnimation,
+  MyLocation
+} from '@ionic-native/google-maps';
+
 @Component({
   selector: 'app-locations',
   templateUrl: './locations.component.html',
   styleUrls: ['./locations.component.scss'],
 })
 export class LocationsComponent implements OnInit {
+  map: GoogleMap;
+  loading: boolean;
 
   constructor() { }
 
-  ngOnInit() {}
+  async ngOnInit() {
+    this.loading = true;
+    await this.loadMap();
+  }
+
+  loadMap() {
+    this.map = GoogleMaps.create('map_canvas', {
+      camera: {
+        target: {
+          lat: 43.0741704,
+          lng: -89.3809802
+        },
+        zoom: 18,
+        tilt: 30
+      }
+    });
+    this.loading = false;
+  }
 
 }
