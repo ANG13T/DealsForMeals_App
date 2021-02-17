dd533e242bc87c9044f0e114e30f1af8fe3d1f02
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-17 21:41:24 +0900

    feat: added new locations code

--- src/app/components/locations/locations.component.ts
+++ src/app/components/locations/locations.component.ts
@@ -1,13 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
-import {
-  GoogleMaps,
-  GoogleMap,
-  GoogleMapsEvent,
-  Marker,
-  GoogleMapsAnimation,
-  MyLocation
-} from '@ionic-native/google-maps';
+import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation'; 
+import { NavController } from '@ionic/angular/providers/nav-controller';
+
+declare var google;
 
 @Component({
   selector: 'app-locations',
@@ -15,28 +11,65 @@ import {
   styleUrls: ['./locations.component.scss'],
 })
 export class LocationsComponent implements OnInit {
-  map: GoogleMap;
-  loading: boolean;
+  options : GeolocationOptions;
+  currentPos : Geoposition;
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  
+  constructor(public navCtrl: NavController,private geolocation : Geolocation) { }
+  
 
-  constructor() { }
-
-  async ngOnInit() {
-    this.loading = true;
-    await this.loadMap();
+  ngOnInit() {
   }
 
-  loadMap() {
-    this.map = GoogleMaps.create('map_canvas', {
-      camera: {
-        target: {
-          lat: 43.0741704,
-          lng: -89.3809802
-        },
-        zoom: 18,
-        tilt: 30
-      }
-    });
-    this.loading = false;
-  }
+  addMap(lat,long){
+
+    let latLng = new google.maps.LatLng(lat, long);
+
+    let mapOptions = {
+    center: latLng,
+    zoom: 15,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.addMarker();
+}
+
+addMarker(){
+
+  let marker = new google.maps.Marker({
+  map: this.map,
+  animation: google.maps.Animation.DROP,
+  position: this.map.getCenter()
+  });
+
+  let content = "<p>This is your current position !</p>";          
+  let infoWindow = new google.maps.InfoWindow({
+  content: content
+  });
+
+  google.maps.event.addListener(marker, 'click', () => {
+  infoWindow.open(this.map, marker);
+  });
+
+}
+
+getUserPosition(){
+  this.options = {
+  enableHighAccuracy : false
+  };
+  this.geolocation.getCurrentPosition(this.options).then((pos : Geoposition) => {
+
+      this.currentPos = pos;     
+
+      console.log(pos);
+      this.addMap(pos.coords.latitude,pos.coords.longitude);
+
+  },(err : PositionError)=>{
+      console.log("error : " + err.message);
+  ;
+  })
+}
 
 }
