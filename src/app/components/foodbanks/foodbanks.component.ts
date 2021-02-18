26b0df23949bf176556a240eb4f75e214e250670
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-18 23:09:55 +0900

    feat: integrated foodbank service

--- src/app/components/foodbanks/foodbanks.component.ts
+++ src/app/components/foodbanks/foodbanks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/models/user.model';
 import { FoodbankService } from 'src/app/shared/services/foodbank.service';
 
 
@@ -9,9 +10,15 @@ import { FoodbankService } from 'src/app/shared/services/foodbank.service';
 })
 export class FoodbanksComponent implements OnInit {
 
+  foodbanks: User[];
+
   constructor(private foodbankService: FoodbankService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.foodbankService.getFoodbanks(5).then((data) => {
+      this.foodbanks = data;
+    })
+  }
 
   
 
