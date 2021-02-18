e98a583048f4f793978ffb98ee4062aa6e274b76
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-18 23:15:39 +0900

    feat: finished stores service

--- src/app/shared/services/store.service.ts
+++ src/app/shared/services/store.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  async getStores(amount: number): Promise<any>{
+    let stores: User[] = [];
+    let promise = this.afs.firestore.collection("users").where("accountType", "==", "store").limit(amount).get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        let data = doc.data();
+        let store: User = {
+          name: data.name,
+          email: data.email,
+          uid: data.uid,
+          accountType: data.accountType
+        }
+        stores.push(store)
+      })
+      return stores;
+    })
+    return promise;
+  }
 }
