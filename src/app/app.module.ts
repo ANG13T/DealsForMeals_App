3ce5601f4007422026a7c286cca2ba8cf8750b31
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-22 00:31:50 +0900

    feat: added ion stepper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,11 +32,12 @@ import { BottomBarComponent } from './components/bottom-bar/bottom-bar.component
 import { FoodbanksComponent } from './components/foodbanks/foodbanks.component';
 import { LocationsComponent } from './components/locations/locations.component';
 import { StoresComponent } from './components/stores/stores.component';
+import { IonicStepperModule } from 'ionic-stepper';
 
 @NgModule({
   declarations: [AppComponent, HomePage, SignUpComponent, SignInComponent, ProfileComponent, SideNavComponent, BaseNavComponent, EditProfileComponent, CreatePostComponent, ViewPostComponent, EditPostComponent, BottomBarComponent, FoodbanksComponent, LocationsComponent, StoresComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, AngularFireModule.initializeApp(environment.firebaseConfig),AngularFireAuthModule, AngularFireStorageModule, FormsModule, ReactiveFormsModule, CommonModule, HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, AngularFireModule.initializeApp(environment.firebaseConfig),AngularFireAuthModule, AngularFireStorageModule, FormsModule, ReactiveFormsModule, CommonModule, HttpClientModule, IonicStepperModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, AngularFirestore, NativeGeocoder, Geolocation],
   bootstrap: [AppComponent],
 })
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -150,6 +150,10 @@ export class SignUpComponent implements OnInit {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
+
+  selectChange(e) {
+    console.log(e);
+  }
   
 
 }
