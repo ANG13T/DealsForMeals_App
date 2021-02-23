7fa73c008c527cbad310637d84c094ae97e23518
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-23 16:29:50 +0900

    feat: added default functionality to view buisness

--- src/app/components/view-buisness/view-buisness.component.ts
+++ src/app/components/view-buisness/view-buisness.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/shared/models/user.model';
+import { BuisnessService } from 'src/app/shared/services/buisness.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-view-buisness',
@@ -10,12 +12,19 @@ export class ViewBuisnessComponent implements OnInit {
 
   @Input() buisness: User;
 
-  constructor() { }
+  constructor(private buisnessService: BuisnessService, private route:ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     if(!this.buisness){
-      // get data from id
+      let buisnessID = this.route.snapshot.params['id'];
+      this.buisnessService.getBuisnessByID(buisnessID).then((buisness) => {
+        this.buisness = buisness;
+      })
     }
+  }
+
+  navigate(route: string){
+    this.router.navigate([`/${route}`]);
   }
 
 }
