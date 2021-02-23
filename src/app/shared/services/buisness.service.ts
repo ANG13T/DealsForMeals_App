afe3b4694e9315d267750212efa46dec31b1e50f
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-23 16:20:38 +0900

    feat: made methods for buisness service

--- src/app/shared/services/buisness.service.ts
+++ src/app/shared/services/buisness.service.ts
@@ -1,9 +1,70 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BuisnessService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  async getBuisnesses(amount: number){
+    let buisnesses: User[] = [];
+    let promise = this.afs.firestore.collection("users").limit(amount).get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        let data = doc.data();
+        let foodbank: User = {
+          name: data.name,
+          email: data.email,
+          uid: data.uid,
+          accountType: data.accountType,
+          photoURL: data.photoURL,
+          isBusiness: data.isBusiness
+        }
+        buisnesses.push(foodbank);
+      })
+      return buisnesses;
+    })
+    return promise;
+  }
+
+  // TODO: make category type specific
+  async getCategoryBuisnesses(amount: number, category: string){
+    let buisnesses: User[] = [];
+    let promise = this.afs.firestore.collection("users").where("accountType", "==", category).limit(amount).get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        let data = doc.data();
+        let foodbank: User = {
+          name: data.name,
+          email: data.email,
+          uid: data.uid,
+          accountType: data.accountType,
+          photoURL: data.photoURL,
+          isBusiness: data.isBusiness
+        }
+        buisnesses.push(foodbank);
+      })
+      return buisnesses;
+    })
+    return promise;
+  }
+
+  async getBuisnessByID(id: string){
+    let buisness: User;
+    let promise = this.afs.firestore.collection("users").doc(id).get().then((doc: any) => {
+        let data = doc.data();
+        buisness = {
+          name: data.name,
+          email: data.email,
+          uid: data.uid,
+          accountType: data.accountType,
+          photoURL: data.photoURL,
+          isBusiness: data.isBusiness
+        } 
+        return buisness;
+    })
+    return promise;
+  }
+
 }
