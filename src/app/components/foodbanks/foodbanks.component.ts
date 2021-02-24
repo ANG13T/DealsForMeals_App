842bb53cfbf0f4fcef8e20777c5b8fa649310079
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-24 22:44:56 +0900

    feat: load deals works

--- src/app/components/foodbanks/foodbanks.component.ts
+++ src/app/components/foodbanks/foodbanks.component.ts
@@ -18,24 +18,39 @@ export class FoodbanksComponent implements OnInit {
 
   foodbanks: User[];
   deals: Post[];
+  loadPosts: any[] = [{id: 1, imageLoaded: false}, {id: 2, imageLoaded: false}, {id: 3, imageLoaded: false}]
+  loadDeals: any[] = [];
+  loadingFoodbanks: boolean = false;
+  loadingDeals: boolean = false;
   showFoodbanks: boolean = true;
 
   constructor(private buisnessService: BuisnessService, private postService: PostService, private routerOutlet: IonRouterOutlet, private modalController: ModalController) { }
 
   ngOnInit() {
+    this.loadingDeals = true;
+    this.loadingFoodbanks = true;
     this.buisnessService.getCategoryBuisnesses(5, 'foodbank').then((data) => {
       console.log("got foodbanks", data)
       this.foodbanks = data;
+      this.loadingFoodbanks = false;
     })
 
     this.postService.getDeals(5).then((data) => {
       console.log("got deals", data);
       this.deals = data;
+      this.setLoadDeals();
+      this.loadingDeals = false;
     })
   }
 
   toggleShowFoodbanks(){
     this.showFoodbanks = !this.showFoodbanks;
+  }
+
+  setLoadDeals(){
+    this.deals.forEach((deal) => {
+      this.loadDeals.push({showImage: false, deal: deal});
+    })
   }
 
 
