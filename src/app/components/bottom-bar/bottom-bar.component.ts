fe55ef9e9df794363a49615004c61950255e9beb
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-27 05:05:42 +0900

    feat: select bottom value nav working

--- src/app/components/bottom-bar/bottom-bar.component.ts
+++ src/app/components/bottom-bar/bottom-bar.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { User } from 'src/app/shared/models/user.model';
 import { UserService } from 'src/app/shared/services/user.service';
@@ -8,19 +8,26 @@ import { CreatePostComponent } from '../modals/create-post/create-post.component
 @Component({
   selector: 'app-bottom-bar',
   templateUrl: './bottom-bar.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./bottom-bar.component.scss'],
 })
 export class BottomBarComponent implements OnInit {
 
   user: User;
-  @Input() selected: string;
+  selectedPath: string;
 
   constructor(public modalController: ModalController, private userService: UserService, private router: Router) {
     this.userService.user$.subscribe(async (userProfile) => {
       if(userProfile){
         this.user = userProfile;
       }
-    })
+    });
+
+    this.router.events.subscribe((event: RouterEvent) => {
+      if(event && event.url){
+        this.selectedPath = event.url;
+      }
+    });
    }
 
   ngOnInit() {}
