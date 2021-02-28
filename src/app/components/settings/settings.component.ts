f1bebc8ea46d1629daf5af54fd86c41520713b8b
angelina-tsuboi <angelina.t1832@gmail.com>
2021-02-28 16:13:51 +0900

    fix: UI errors with profile page

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {}
+
+  navigate(route: string){
+    this.router.navigate([`/${route}`]);
+  }
 
 }
