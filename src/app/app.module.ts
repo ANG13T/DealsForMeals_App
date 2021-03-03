8e6feb62c816416462bf5015ed38999b42958a10
angelina-tsuboi <angelina.t1832@gmail.com>
2021-03-04 05:41:06 +0900

    feat: made minor UX changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,7 +50,7 @@ import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [AppComponent, HomePage, SignUpComponent, SignInComponent, ProfileComponent, SideNavComponent, BaseNavComponent, EditProfileComponent, CreatePostComponent, EditPostComponent, BottomBarComponent, FoodbanksComponent, LocationsComponent, StoresComponent, ViewBuisnessComponent, ViewDealComponent, SettingsComponent, CreatePostComponent],
-  entryComponents: [],
+  entryComponents: [CreatePostComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, AngularFireModule.initializeApp(environment.firebaseConfig),AngularFireAuthModule, AngularFireStorageModule, FormsModule, ReactiveFormsModule, CommonModule, HttpClientModule, MatStepperModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatTabsModule, MatIconModule, AgmCoreModule.forRoot({
     apiKey: environment.googleMapsAPIKey
   }), AvatarModule],
