18a3c3f3e2a77a4c7a47813210fee866737e89f3
angelina-tsuboi <angelina.t1832@gmail.com>
2021-03-07 05:44:44 +0900

    feat: removed geofirestore

--- src/app/shared/services/buisness.service.ts
+++ src/app/shared/services/buisness.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { User } from '../models/user.model';
 import * as firebase from 'firebase/app';
-import { GeoFirestore } from 'geofirestore';
+// import { GeoFirestore } from 'geofirestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BuisnessService {
 
-  loadGeoFire(){
-    const geoFire = new GeoFirestore(firebase.default.firestore());  
-    geoFire.collection("users")
-  }
 
   constructor(private afs: AngularFirestore) { 
-    this.loadGeoFire();
+  
+  }
+
+  async getBuisnessesNearLocation(loca){
+
   }
 
   async getBuisnesses(amount: number){
