d68cfc5a214df4755cf43c521957e1c411777521
angelina-tsuboi <angelina.t1832@gmail.com>
2021-03-08 10:07:20 +0900

    chore: cleaned up app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
+
+import { RouteReuseStrategy } from '@angular/router';
+import { environment } from 'src/environments/environment';
+
+//  Geolocation
+import { NativeGeocoder, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+// Modules
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';  
-import { RouteReuseStrategy } from '@angular/router';
-
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { MaterialModule } from './shared/modules/material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AgmCoreModule } from '@agm/core';
+import { AvatarModule } from 'ngx-avatar';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
-import { environment } from 'src/environments/environment';
-import { NativeGeocoder, NativeGeocoderOptions } from '@ionic-native/native-geocoder/ngx';
-import { Geolocation } from '@ionic-native/geolocation/ngx';
 
-import { AngularFireAuthModule } from '@angular/fire/auth';
-import { AngularFireStorageModule } from '@angular/fire/storage';
-import { MaterialModule } from './shared/modules/material/material.module';
-import { HttpClientModule } from '@angular/common/http'; 
-import { AngularFirestore } from '@angular/fire/firestore';
+// Components
+import { AppComponent } from './app.component';
 import { HomePage } from './components/home/home.page';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
@@ -32,12 +38,8 @@ import { BottomBarComponent } from './components/bottom-bar/bottom-bar.component
 import { FoodbanksComponent } from './components/foodbanks/foodbanks.component';
 import { LocationsComponent } from './components/locations/locations.component';
 import { StoresComponent } from './components/stores/stores.component';
-
-// Material Imports
 import { ViewBuisnessComponent } from './components/view-buisness/view-buisness.component';
 import { ViewDealComponent } from './components/view-deal/view-deal.component';
-import { AgmCoreModule } from '@agm/core';
-import { AvatarModule } from 'ngx-avatar';
 import { SettingsComponent } from './components/settings/settings.component';
 import { ChipsSelectComponent } from './components/chips-select/chips-select.component';
 
@@ -46,10 +48,10 @@ import { ChipsSelectComponent } from './components/chips-select/chips-select.com
 @NgModule({
   declarations: [AppComponent, HomePage, SignUpComponent, SignInComponent, ProfileComponent, SideNavComponent, BaseNavComponent, EditProfileComponent, CreatePostComponent, EditPostComponent, BottomBarComponent, FoodbanksComponent, LocationsComponent, StoresComponent, ViewBuisnessComponent, ViewDealComponent, SettingsComponent, CreatePostComponent, ChipsSelectComponent],
   entryComponents: [CreatePostComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, AngularFireModule.initializeApp(environment.firebaseConfig),AngularFireAuthModule, AngularFireStorageModule, FormsModule, ReactiveFormsModule, CommonModule, HttpClientModule, MaterialModule, AgmCoreModule.forRoot({
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireAuthModule, AngularFireStorageModule, FormsModule, ReactiveFormsModule, CommonModule, HttpClientModule, MaterialModule, AgmCoreModule.forRoot({
     apiKey: environment.googleMapsAPIKey
   }), AvatarModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, AngularFirestore, NativeGeocoder, Geolocation],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
