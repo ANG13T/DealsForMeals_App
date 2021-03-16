89d1bcb70e0bdb12099839e993a8aa961974adc9
angelina-tsuboi <angelina.t1832@gmail.com>
2021-03-17 00:58:30 +0900

    feat: finished buisness category pipe

--- src/app/shared/pipes/buisness-category.pipe.ts
+++ src/app/shared/pipes/buisness-category.pipe.ts
@@ -1,12 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { User } from '../models/user.model';
 
 @Pipe({
   name: 'buisnessCategory'
 })
 export class BuisnessCategoryPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(buisnesses: User[], categories: string[]) {
+    let lowercasedCategories = categories.map(cat => cat.toLowerCase())
+    let modifiedCategories = lowercasedCategories.map(cat => {
+      if(cat[cat.length - 1] == "s"){
+        return cat.slice(0, cat.length - 1);
+      }
+      return cat;
+    })
+    return buisnesses.filter(buisness => modifiedCategories.includes(buisness.accountType));
   }
 
 }
