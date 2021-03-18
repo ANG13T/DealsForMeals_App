4119b953b22b864538b6533f455d7fe1b37fa72f
angelina-tsuboi <angelina.t1832@gmail.com>
2021-03-18 22:04:09 +0900

    feat: use private google maps api

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+let googleMapsAPI = `http://maps.google.com/maps/api/js?libraries=places&key=${environment.googleMapsAPIKey}`;
+
 if (environment.production) {
   enableProdMode();
 }
