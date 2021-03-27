cf478a487ce81e4ac3bf0dac6fc392937b6fdc8f
angelina-tsuboi <angelina.t1832@gmail.com>
2021-03-27 23:53:28 +0900

    feat: created send email function in fb functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,43 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const cors = require('cors')({origin: true});
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+/**
+* Here we're using Gmail to send 
+*/
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'yourgmailaccount@gmail.com',
+        pass: 'yourgmailaccpassword'
+    }
+});
+
+exports.sendMail = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+      
+        // getting dest email by query string
+        const dest = req.query.dest;
+        const message = req.query.message;
+
+        const mailOptions = {
+            from: 'Your Account Name <yourgmailaccount@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
+            to: dest,
+            subject: 'I\'M A PICKLE!!!', // email subject
+            html: `<p style="font-size: 16px;">Pickle Riiiiiiiiiiiiiiiick!!</p>
+                <br />
+                <img src="https://images.prod.meredith.com/product/fc8754735c8a9b4aebb786278e7265a5/1538025388228/l/rick-and-morty-pickle-rick-sticker" />
+            ` // email content in HTML
+        };
+  
+        // returning result
+        return transporter.sendMail(mailOptions, (erro, info) => {
+            if(erro){
+                return res.send(erro.toString());
+            }
+            return res.send('Sended');
+        });
+    });    
+});
