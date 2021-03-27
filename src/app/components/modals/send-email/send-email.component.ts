536592cd7688d8e2eee2f0dc11da9b83d57027ce
angelina-tsuboi <angelina.t1832@gmail.com>
2021-03-27 23:46:04 +0900

    feat: set up templating and logic for send email

--- src/app/components/modals/send-email/send-email.component.ts
+++ src/app/components/modals/send-email/send-email.component.ts
@@ -7,10 +7,19 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./send-email.component.scss'],
 })
 export class SendEmailComponent implements OnInit {
-  message:string;
+  message:string = "";
 
   constructor(@Inject(MAT_DIALOG_DATA) public data) { }
 
   ngOnInit() {}
+
+  sendEmail(){
+    if(this.message.length < 20){
+      alert("Please type atleast 20 characters");
+      return;
+    }
+
+    
+  }
 
 }
