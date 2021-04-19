46618e343e0cc5f8e4a7b367b78f99401261e416
angelina-tsuboi <angelina.t1832@gmail.com>
2021-04-19 23:37:00 +0900

    feat: remove api keys

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// let googleMapsAPI = `http://maps.google.com/maps/api/js?libraries=places&key=${environment.googleMapsAPIKey}`;
-let googleMapsAPI = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsAPIKey}&libraries=places&sensor=false&language=en`
 
 if (environment.production) {
   enableProdMode();
